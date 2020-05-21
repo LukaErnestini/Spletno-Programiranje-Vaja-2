352ae81afd91748338f27022a5a6d2288760d135
Luka Ernestini <luka.ernestini@gmail.com>
2020-05-21 14:03:07 +0200

    added nav

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -1,8 +1,27 @@
-<nav>
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
-    <li><a href="/users">Users</a></li>
-  </ul
-</nav>
+<div class="container">
+  <nav class="navbar navbar-expand-sm bg-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Domov</a>
+      </li>
+      <?php	if(isset($_SESSION["USER_ID"])){ ?>
+        <li class="nav-item">
+          <a class="nav-link" href="objavi.php">Objavi oglas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="odjava.php">Odjava</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"href="mojiOglasi.php">Moji oglasi</a>
+        </li>
+      <?php } else{ ?>
+        <li class="nav-item">
+          <a class="nav-link" href="prijava.php">Prijava</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="registracija.php">Registracija</a>
+        </li>
+      <?php	}	?>
+    </ul>
+  </nav>
+</div>
